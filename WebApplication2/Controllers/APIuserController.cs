ad4f589df6b145144a609f5eb6bbf78c6c7fe2cc
Trần Tuấn Linh <gunnyonline096@gmail.com>
2019-11-22 08:19:02 +0700

    19.11.21

---
+++ WebApplication2/Controllers/APIuserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+//using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+
+    public class APIuserController : ApiController
+    {
+        private PasGoEntities2 db = new PasGoEntities2();
+
+
+        public PasgoRestaurant Get(int PasgoRID)
+        {
+            var result = db.FullInfoRestaurant(PasgoRID).ToList().ElementAt(0);
+            return result;
+        }
+            
+    }
+}
